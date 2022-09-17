005109539f1a7256308daba9d243b8010698ffab
evgeshabond <eugeniy_geniy@mail.ru>
2022-09-17 17:55:09 +0200

    configure extensions to work with twin

--- src/header/Logo.tsx
+++ src/header/Logo.tsx
@@ -1,7 +1,9 @@
 export const Logo = () => {
+  // const lol = tw.div`flex`
+
   return (
-    <div tw="flex h-full items-center bg-grey-dark px-[77px] py-[17px] flex-shrink-[0]">
-      <span tw="font-size[20px] font-weight[600] line-height[36px] uppercase letter-spacing[1px] height[300px]">
+    <div tw="flex h-full items-center bg-grey-dark px-[77px] py-[17px] text-white">
+      <span tw="font-size[20px] font-weight[600] line-height[36px] uppercase letter-spacing[1px]">
         Aufgabe
       </span>
     </div>
